7f0939d73d80810dc72a18372041dc3ae4621d73
Alisa Mordvinova <aurumkes@gmail.com>
2026-02-18 17:07:56 +0300

    Step 4.2.6. Check element on the page with implictly_wait and tru-except-retern

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,3 +1,5 @@
+from selenium.common.exceptions import InvalidSelectorException
+from selenium.webdriver import Remote as RemoteWebDriver
 # базовая страница, от которой будут унаследованы все остальные классы. В ней  описаны вспомогательные методы для работы с драйвером
 
 class BasePage():
@@ -6,9 +8,10 @@ class BasePage():
 
     # self как this
 
-    def __init__(self, browser, url):
+    def __init__(self, browser, url, timeout=10):
         self.browser = browser
         self.url = url
+        self.browser.implicitly_wait(timeout)
         
     # Теперь добавим еще один метод open. Он должен открывать нужную страницу в браузере, используя метод get(). Объявите ниже в том же классе:
     def open(self):
@@ -17,4 +20,9 @@ class BasePage():
     # В __init__ мы сохраняем данные (browser и url)
     # В open мы используем сохраненные данные через self
 
-
+    def is_element_present(self, how, what):
+        try:
+            self.browser.find_element(how, what)
+        except (InvalidSelectorException):
+            return False
+        return True
--- pages/main_page.py
+++ pages/main_page.py
@@ -1,7 +1,10 @@
-from .base_page import BasePage
+from .base_page import BasePage    # Точка . означает "в этой же папке"
 from selenium.webdriver.common.by import By
 
 class MainPage(BasePage):
     def go_to_login_page(self):
         login_link = self.browser.find_element(By.CSS_SELECTOR, "#login_link")
         login_link.click()
+
+    def should_be_login_link(self): # метод проверки should_be_(название элемента)
+        assert self.browser.find_element(By.CSS_SELECTOR, "#login_link"), "Login link is not presented"
