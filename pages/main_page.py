aa90f4e68e1b5d75d3beadf093649f0d88bc199e
Alisa Mordvinova <aurumkes@gmail.com>
2026-02-19 18:58:39 +0300

    Урок 4.2.7 Элементы страниц в паттерне Page Object. locators.py

--- pages/main_page.py
+++ pages/main_page.py
@@ -1,8 +1,10 @@
 from .base_page import BasePage    # Точка . означает "в этой же папке"
+#from .locators import MainPageLocators
 from selenium.webdriver.common.by import By
 
 class MainPage(BasePage):
     def go_to_login_page(self):
+        #assert self.is_element_present(*MainPageLocators.LOGIN_LINK), "Login link is not presented" # символ * указывает на то, что мы передали именно пару, и этот кортеж нужно распаковать
         login_link = self.browser.find_element(By.CSS_SELECTOR, "#login_link")
         login_link.click()
 
