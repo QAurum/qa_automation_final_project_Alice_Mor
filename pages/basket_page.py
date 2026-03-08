c16144eb6b9228b85503d0004afbb5a20025a913
A.Mordvinova <aurumkes@gmail.com>
2026-03-08 17:03:04 +0300

    А вот и basket_page.py

---
+++ pages/basket_page.py
@@ -0,0 +1,22 @@
+from .locators import BasePageLocators, BasketPageLocators
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.common.exceptions import InvalidSelectorException, NoSuchElementException, NoAlertPresentException, TimeoutException
+from selenium.webdriver.support import expected_conditions as EC
+import math
+# базовая страница, от которой будут унаследованы все остальные классы. В ней  описаны вспомогательные методы для работы с драйвером
+
+class BasketPage():
+
+# Методы-действия (actions) — без assert
+
+
+
+# Методы-проверки (assertions) — с assert
+
+    def should_not_be_products_in_basket(self):
+        assert self.is_not_element_present(*BasketPageLocators.BASKET_ITEMS), \
+        "❌ Find items in basket. WHY?"
+
+    def should_see_message_that_basket_is_empty(self):
+        assert self.is_element_present(*BasketPageLocators.EMPTY_BASKET), \
+        "❌ Messege about empty basket not found"
