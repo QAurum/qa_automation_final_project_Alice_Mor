2266d59598c6202fe5f4243d2fbef1d6ac0e0d78
Alisa Mordvinova <aurumkes@gmail.com>
2026-03-24 18:42:24 +0300

    Мелкие доработки

--- conftest.py
+++ conftest.py
@@ -4,6 +4,10 @@ import os
 import pytest
 from selenium import webdriver
 
+chrome_options = ChromeOptions()
+chrome_options.add_argument('--headless') # позволяет запускать тесты в фоне без графического интерфейса. Это ускоряет парсинг сайтов и автоматизацию тестирования на серверах, экономя ресурсы.
+# options.add_argument("--window-position=-2400,-2400") #для использования на версии хрома 29,30. Так как в селениуме открывается просто пустое окно, если хедлесс не указать без второй строчки
+
 def pytest_addoption(parser):
     parser.addoption('--language', action='store', default=None,
                      help="You need to choose language: es, fr, ru, etc.")
